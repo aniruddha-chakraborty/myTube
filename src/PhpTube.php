8d2ac744270896fac9ba67648795e6a80701deb5
aniruddha-chakraborty <aniruddha.app@gmail.com>
2015-11-25 19:19:52 +0600

    added composer.json

---
+++ src/PhpTube.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @package PhpTube - A PHP class to get youtube download links
+ * @author Abu Ashraf Masnun <mailbox@masnun.me>
+ * @website http://masnun.com
+ *
+ */
+
+abstract class PhpTube
+{
+
+abstract public function getHtml($url);
+
+}
